f521dd07664ac0f8c008681812ee2ebfb8c039cf
Bardo91 <19bardo91@gmail.com>
2021-11-23 00:40:19 +0100

    WIP linking libraries

--- android/src/main/cpp/gstreamer_launch_android_native.cpp
+++ android/src/main/cpp/gstreamer_launch_android_native.cpp
@@ -1,11 +1,12 @@
 #include <jni.h>
 #include <string>
+#include <gst/gst.h>
 
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_bardo91_gstreamer_1launch_GstreamerLaunchPlugin_stringFromJNI(
         JNIEnv* env,
         jobject /* this */) {
-    std::string hello = "Hello from C++";
+    std::string hello = gst_version_string();
     return env->NewStringUTF(hello.c_str());
 }
 
